bc902ebb7dfc67dfc2d6fc16b4799e3ae16bd6fd
= <=>
2016-05-03 17:48:34 -0500

    Sentiment analysis code and gitignore

--- src/main.py
+++ src/main.py
@@ -1,28 +1,48 @@
 import filter
 import tweet
 import TweetReader
+from os.path import exists
 
-# filenmae
 
-filePath = "/Users/anirudhnair/Dropbox/WORK/lecNotes/cs410/project/test.json"
-configXml = "/Users/anirudhnair/Documents/workspace/TwitterAnalysis/filter.xml"
-outFile = "/Users/anirudhnair/Dropbox/WORK/lecNotes/cs410/project/test_out.json"
-reader = TweetReader.TweetReader(filePath)
-filter_ = filter.Filter(configXml)
-oFP = open(outFile,'w')
-totalTweets = 0
-totalFilteredTweets = 0
-while (True):
-    tweet_ = reader.next()
-    if (tweet_ == None):
-        break
-    totalTweets = totalTweets + 1
-    if( filter_.FilterTweet(tweet_) ):
-        oFP.write(tweet_.GetJSONStr())
-        totalFilteredTweets = totalFilteredTweets + 1
-        
-        
-        
-oFP.close()
-print "Total tweets processed " + str(totalTweets) + " \n"
-print "Total tweets that passed the filter " + str(totalFilteredTweets) + " \n"
+def calculate_and_dump_sentiment():
+    for month in ['Mar']:
+        for day in range(1, 31):
+            fname = './../../tweets/{}/{}.json'.format(month, "%02d" % day)
+            if not exists(fname):
+                continue
+            print "Starting", month, day
+            reader = TweetReader.TweetReader(fname)
+            save_fname = fname[:-5] + '_filtered_analyzed' + fname[-5:]
+            while True:
+                curr_tweet = reader.next()
+                if curr_tweet is None:
+                    break
+                curr_tweet.dumpSentimentJSON(save_fname)
+
+
+def filter_tweets():
+
+    filePath = "/Users/anirudhnair/Dropbox/WORK/lecNotes/cs410/project/test.json"
+    configXml = "/Users/anirudhnair/Documents/workspace/TwitterAnalysis/filter.xml"
+    outFile = "/Users/anirudhnair/Dropbox/WORK/lecNotes/cs410/project/test_out.json"
+    reader = TweetReader.TweetReader(filePath)
+    filter_ = filter.Filter(configXml)
+    oFP = open(outFile, 'w')
+    totalTweets = 0
+    totalFilteredTweets = 0
+    while (True):
+        tweet_ = reader.next()
+        if (tweet_ is None):
+            break
+        totalTweets = totalTweets + 1
+        if(filter_.FilterTweet(tweet_)):
+            oFP.write(tweet_.GetJSONStr())
+            totalFilteredTweets = totalFilteredTweets + 1
+
+    oFP.close()
+    print "Total tweets processed " + str(totalTweets) + " \n"
+    print "Total tweets that passed the filter " + str(totalFilteredTweets) + " \n"
+
+if __name__ == "__main__":
+	# filter_tweets()
+    calculate_and_dump_sentiment()
--- src/tweet.py
+++ src/tweet.py
@@ -8,61 +8,59 @@ sid = SentimentIntensityAnalyzer()
 
 
 class Tweet:
-    
-    def __init__(self,tweet_json):
+
+    def __init__(self, tweet_json):
         self.json_str = tweet_json
         tweet = json.loads(tweet_json)
         self.id = tweet['id_str']
         # read tweet
         self.text = tweet['text']
-        #read hashtags
+        # read hashtags
         if tweet['entities']['hashtags']:
             self.hashtags = []
-            hashtag_objs =  tweet['entities']['hashtags'] 
+            hashtag_objs = tweet['entities']['hashtags']
             for hashtag in hashtag_objs:
                 self.hashtags.append(hashtag['text'])
         else:
             self.hashtags = None
-        #read mentions
+        # read mentions
         if tweet['entities']['user_mentions']:
-            self.user_mentions =  []
-            user_mention_obj = tweet['entities']['user_mentions'] 
+            self.user_mentions = []
+            user_mention_obj = tweet['entities']['user_mentions']
             for user_mention in user_mention_obj:
                 self.user_mentions.append(user_mention['screen_name'])
         else:
             self.user_mentions = None
-        #read date
+        # read date
         self.date = parser.parse(tweet['created_at'])
-        
-        #read location
+
+        # read location
         if tweet['place']:
             self.location = tweet['place']['full_name']
         else:
             self.location = None
-        
-        
+
     def GetJSONStr(self):
         return self.json_str
-    
+
     def GetTweet(self):
         return self.text
-    
+
     def GetHashTags(self):
         return self.hashtags
-        
+
     def GetUserMentions(self):
         return self.user_mentions
-        
+
     def GetDate(self):
         return self.date
-        
+
     def GetLocation(self):
         return self.location
 
     def GetSentiment(self):
         sentiments = sid.polarity_scores("The food is delicious")
         return (sentiments["pos"], sentiments["neg"])
-
 
     def createObj(self):
         obj = {}
@@ -71,9 +69,9 @@ class Tweet:
         obj["user_mentions"] = self.GetUserMentions()
         obj["hashtags"] = self.GetHashTags()
         obj["sentiment"] = self.GetSentiment()
-        obj["date"] = self.GetDate()
+        obj["date"] = str(self.GetDate())
         return obj
 
     def dumpSentimentJSON(self, fname):
         with open(fname, "a") as outFile:
-            myfile.write(json.dumps(outFile))
+            outFile.write(json.dumps(self.createObj()))
